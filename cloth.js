9c50f49751a933292f24b37308fe7765f72e7545
BuildTools <davidmaamoaix@gmail.com>
2022-03-05 10:13:00 +0800

    massparam

--- cloth.js
+++ cloth.js
@@ -1,13 +1,17 @@
 const unit = Math.min(window.innerWidth, window.innerHeight) / 12.5;
 const CONFIG = {
     mouseDragForce: 1,
-    dampen: 0.99,
+    dragRadius: 100,
+    dampen: 0.95,
     stiffness: 6.5,
-    nodeCount: 11,
+    nodeCountX: 15,
+    nodeCountY: 9,
     nodeDist: unit,
     nodeSize: 5,
-    gravity: 20,
-    deltaTime: 0.1
+    gravity: 9.81,
+    nodeMass: 1,
+    deltaTime: 0.1,
+    yOffset: -50
 };
 
 class Node {
@@ -41,6 +45,7 @@ class Node {
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const nodes = [];
+let [mouseX, mouseY] = [0, 0];
 
 function norm(x, y) {
     return Math.sqrt(x * x + y * y);
@@ -69,19 +74,20 @@ function drawLine(from, to) {
 function mapNeighbor(i, j, func) {
     if (i !== 0) func(nodes[i - 1][j]);
     if (j !== 0) func(nodes[i][j - 1]);
-    if (i !== CONFIG.nodeCount - 1) func(nodes[i + 1][j]);
-    if (j !== CONFIG.nodeCount - 1) func(nodes[i][j + 1]);
+    if (i !== CONFIG.nodeCountY - 1) func(nodes[i + 1][j]);
+    if (j !== CONFIG.nodeCountX - 1) func(nodes[i][j + 1]);
 }
 
 function updateCanvas() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 1;
 
-    for (var i = 0; i < CONFIG.nodeCount; i++) {
-        for (var j = 0; j < CONFIG.nodeCount; j++) {
+    for (var i = 0; i < CONFIG.nodeCountY; i++) {
+        for (var j = 0; j < CONFIG.nodeCountX; j++) {
             const node = nodes[i][j];
 
             let [fx, fy] = [0, 0];
-            fy += CONFIG.gravity;
+            fy += CONFIG.gravity * CONFIG.nodeMass;
             mapNeighbor(i, j, other => {
                 const [dx, dy] = [other.x - node.x, other.y - node.y];
                 const magnitude = norm(dx, dy) - CONFIG.nodeDist;
@@ -92,20 +98,29 @@ function updateCanvas() {
                 fy += dfy;
             });
 
-            node.addVel(fx * CONFIG.deltaTime, fy * CONFIG.deltaTime);
+            node.addVel(
+                fx * CONFIG.deltaTime / CONFIG.nodeMass,
+                fy * CONFIG.deltaTime / CONFIG.nodeMass
+            );
             node.updatePos();
 
-            if (i !== CONFIG.nodeCount - 1) {
+            if (i !== CONFIG.nodeCountY - 1) {
                 drawLine(node, nodes[i + 1][j]);
             }
 
-            if (j !== CONFIG.nodeCount - 1) {
+            if (j !== CONFIG.nodeCountX - 1) {
                 drawLine(node, nodes[i][j + 1]);
             }
 
             drawNode(node);
         }
     }
+
+    ctx.strokeStyle = '#0078CE';
+    ctx.lineWidth = 3;
+    ctx.beginPath();
+    ctx.arc(mouseX, mouseY, CONFIG.dragRadius, 0, 2 * Math.PI);
+    ctx.stroke();
 }
 
 (function() {
@@ -117,14 +132,14 @@ function updateCanvas() {
     window.addEventListener('resize', updateCanvasGeometry);
 
     const [midX, midY] =  [canvas.width / 2, canvas.height / 2];
-    const top = midY - (CONFIG.nodeCount - 1) * CONFIG.nodeDist / 2;
-    const left = midX - (CONFIG.nodeCount - 1) * CONFIG.nodeDist / 2;
-    for (var i = 0; i < CONFIG.nodeCount; i++) {
+    const top = midY - (CONFIG.nodeCountY - 1) * CONFIG.nodeDist / 2;
+    const left = midX - (CONFIG.nodeCountX - 1) * CONFIG.nodeDist / 2;
+    for (var i = 0; i < CONFIG.nodeCountY; i++) {
         const row = [];
-        for (var j = 0; j < CONFIG.nodeCount; j++) {
+        for (var j = 0; j < CONFIG.nodeCountX; j++) {
             const node = new Node(
                 left + j * CONFIG.nodeDist,
-                top + i * CONFIG.nodeDist,
+                top + i * CONFIG.nodeDist + CONFIG.yOffset,
                 i === 0 && (j & 1) === 0
             );
             row.push(node);
@@ -132,18 +147,20 @@ function updateCanvas() {
         nodes.push(row);
     }
 
-    let [prevX, prevY] = [midX, midY];
     canvas.addEventListener('mousemove', e => {
         const [currX, currY] = [
             e.pageX - canvas.offsetLeft,
             e.pageY - canvas.offsetTop
         ];
-        const [dirX, dirY] = [currX - prevX, currY - prevY];
-        [prevX, prevY] = [currX, currY];
+        const [dirX, dirY] = [currX - mouseX, currY - mouseY];
+        [mouseX, mouseY] = [currX, currY];
 
-        for (var i = 0; i < CONFIG.nodeCount; i++) {
-            for (var j = 0; j < CONFIG.nodeCount; j++) {
-                nodes[i][j].addVel(dirX, dirY);
+        for (var i = 0; i < CONFIG.nodeCountY; i++) {
+            for (var j = 0; j < CONFIG.nodeCountX; j++) {
+                const node = nodes[i][j];
+                if (norm(node.x - currX, node.y - currY) < CONFIG.dragRadius) {
+                    node.addVel(dirX, dirY);
+                }
             }
         }
     });
